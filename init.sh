ed92e25961a38dc8c0472d2a7507c73b5a9d433d
tomas_eid <tomas.kunka@gmail.com>
2017-12-14 09:34:29 +0100

    nvim

--- init.sh
+++ init.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 echo "-= Installing System Dependencies =-"
+brew install zsh tmux neovim/neovim/neovim python3 ag reattach-to-user-namespace
+brew tap caskroom/cask
+brew cask install iterm2
 
-sudo apt-get install -y zsh
+echo "-= Upgrading Vim with Package Manager =-"
+curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
+https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+pip3 install neovim
+
+brew tap caskroom/fonts
+brew cask install font-meslo-for-powerline
 
 echo "-= Assigning Zsh as Default Shell =-"
 chsh -s $(which zsh)
 
-echo "-= Upgrading Vim with Package Manager =-"
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs \
-    https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+rm -rf ~/.vim ~/.vimrc ~/.zshrc ~/.tmux ~/.tmux.conf ~/.config/nvim 2> /dev/null
+mkdir -p ~/.config ~/.config/nvim
 
 echo "-= Symlinking new configs =-"
+
 ln -s ~/dotfiles/zshrc ~/.zshrc
-ln -s ~/dotfiles/vimrc ~/.vimrc
+ln -s ~/dotfiles/tmux.conf ~/.tmux.conf
+ln -s ~/dotfiles/vimrc ~/.config/nvim/init.vim
 ln -s ~/dotfiles/vimrc.bundles ~/.vimrc.bundles
 
 echo "-= Log out and Log Back In to see changes =-"
