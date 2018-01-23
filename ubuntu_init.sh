9272ca0e87b17beb72867cc1fa8e1762c557477d
tomas_eid <tomas.kunka@gmail.com>
2018-01-23 11:10:57 +0100

    del newlines

--- ubuntu_init.sh
+++ ubuntu_init.sh
@@ -2,18 +2,18 @@
 echo "-= Installing System Dependencies =-"
 apt-get update && apt-get install -y software-properties-common
 add-apt-repository ppa:neovim-ppa/stable
-apt-get update && apt-get install -y \ 
-  curl \ 
-  zsh \ 
-  tmux \ 
-  neovim \ 
-  python3 \ 
-  python3-pip \ 
-  silversearcher-ag \ 
-  ranger \ 
-  terminator \ 
-  clang \ 
-  ctags \ 
+apt-get update && apt-get install -y \
+  curl \
+  zsh \
+  tmux \
+  neovim \
+  python3 \
+  python3-pip \
+  silversearcher-ag \
+  ranger \
+  terminator \
+  clang \
+  ctags \
   node
 
 source init.sh
