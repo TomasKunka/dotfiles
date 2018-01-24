3006f6d6967fb8b54f07a72be7fc82e5411e2841
tomas_eid <tomas.kunka@gmail.com>
2018-01-24 22:34:41 +0100

    testing tmux from source, neovim path

--- init_debian8.sh
+++ init_debian8.sh
@@ -27,7 +27,7 @@ apt-get install \
   unzip
 
 # install
-git clone https://github.com/neovim/neovim.git && cd neovim
+git clone https://github.com/neovim/neovim.git ~/neovim && cd ~/neovim
 VERSION=0.2.2 && git checkout v${VERSION}
 ./configure && make -j"$(nproc)" && make install
 cd && rm -rf neovim
